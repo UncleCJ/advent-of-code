13aa68e5b15319a5e8d8c5b299bc122f43fd01bb
BNAndras <20251272+BNAndras@users.noreply.github.com>
2020-12-12 22:08:19 -0800

    tweak

--- 2020/12/puzzle.py
+++ 2020/12/puzzle.py
@@ -15,11 +15,11 @@ currentShipPosition = Position(0, 0)
 currentGeographicDirection = 90 # east
 for movement in movements:
     if movement.direction == "F":
-        turnDegrees = currentGeographicDirection % 360
-        if turnDegrees == 0: movement = Movement("N", movement.amount)
-        elif turnDegrees == 90: movement = Movement("E", movement.amount)
-        elif turnDegrees == 180: movement = Movement("S", movement.amount)
-        elif turnDegrees == 270: movement = Movement("W", movement.amount)
+        currentGeographicDirection = currentGeographicDirection % 360
+        if currentGeographicDirection == 0: movement = Movement("N", movement.amount)
+        elif currentGeographicDirection == 90: movement = Movement("E", movement.amount)
+        elif currentGeographicDirection == 180: movement = Movement("S", movement.amount)
+        elif currentGeographicDirection == 270: movement = Movement("W", movement.amount)
     if movement.direction == "N":
         currentShipPosition = Position(currentShipPosition.X, currentShipPosition.Y + movement.amount)
     elif movement.direction == "S":
@@ -60,6 +60,5 @@ for movement in movements:
             movement = Movement(movement.direction, movement.amount - 90)
             currentWaypointPosition = Position(X = currentWaypointPosition.Y, Y = -currentWaypointPosition.X)
 
-
 manhattanDistanceForShipPart2 = abs(currentShipPosition.X) + abs(currentShipPosition.Y)
-print("Part Two : {}".format(manhattanDistanceForShipPart2)) #34198 too high
+print("Part Two : {}".format(manhattanDistanceForShipPart2))
