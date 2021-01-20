decddeef61d7966e9902dc7cbcc8102584e83bd1
CJ Sveningsson <cj.sveningsson@gmail.com>
2021-01-20 21:50:30 +0100

    WIP: More work on shared utilities

--- common.py
+++ common.py
@@ -1,4 +1,7 @@
+import os
 import requests
+
+PROPERTIES = dict()
 
 # By "Roberto" from https://stackoverflow.com/a/31852401https://stackoverflow.com/a/31852401
 def load_properties(filepath, sep='=', comment_char='#'):
@@ -16,37 +19,80 @@ def load_properties(filepath, sep='=', comment_char='#'):
                 props[key] = value 
     return props
 
-def download(year, day, USER_SESSION_ID='', USER_AGENT='adventofcode_notebook_helper', baseurl='https://adventofcode.com/'):
-    output = {}
+def load_config_ini(configfile='../../config.ini')
+    if 'USER_SESSION_ID' in PROPERTIES.keys():
+        return None
+    
     try:
-        with requests.get(url=baseurl + str(year) + '/day/' + str(day),
+        PROPERTIES = load_properties(configfile)
+        return True
+    except:
+        return None
+
+def which_day():
+    cwd_year, day_str = os.path.split(os.getcwd())
+    _, year_str = os.path.split(cwd_year)
+
+    try:
+        year, day = int(year_str), int(day_str)
+        return year, day
+    except:
+        return None
+
+def download_helper(url, USER_SESSION_ID, USER_AGENT):
+    try:
+        with requests.get(url=url,
                           cookies={'session': USER_SESSION_ID},
                           headers={'User-Agent': USER_AGENT}) as response:
             if response.ok:
-                html = response.text
-
-                p1_start = html.find('<article')
-                p1_end = html.find('</article>') + len('</article>')
-                
-                output['part1'] = html[p1_start:p1_end]
-
-                if USER_SESSION_ID != '':
-                    p2_start = html.find('<article', p1_end + 1)
-                    p2_end = html.find('</article>', p1_end + 1) + len('</article>')
-                    if p2_start and p2_end:
-                        output['part2'] = html[p2_start:p2_end]
-        if USER_SESSION_ID != '':
-            with requests.get(url=baseurl + str(year) + '/day/' + str(day) + '/input',
-                              cookies={'session': USER_SESSION_ID},
-                              headers={'User-Agent': USER_AGENT}) as response:
-                if response.ok:
-                    input = response.text
-                    if input:
-                        output['input'] = input.rstrip('\n')
-        return output
+                return response.text
     except requests.exceptions.RequestException:
         print("Error while requesting statement from server. Request probably timed out. Giving up.")
         return None
     except Exception as e:
         print("Non handled error while requesting statement from server. " + str(e))
         return None
+
+def download(year, day,
+             USER_SESSION_ID='',
+             USER_AGENT='adventofcode_notebook_helper',
+             baseurl='https://adventofcode.com/'):
+    output = dict()
+
+    # Download any problem statements and footers we can
+    html = download_helper(baseurl + str(year) + '/day/' + str(day),
+                           USER_SESSION_ID, USER_AGENT)    
+    if html:
+        p1_start = html.find('<article')
+        # str.find returns -1 if not found, so this arithmetic is sloppy
+        p1_end = html.find('</article>') + len('</article>')
+        p1_footer_start = p1_end + 1
+        bottom_end = html.find('</main>', p1_footer_start) - 1
+        
+        output['part1'] = html[p1_start:p1_end]
+
+        if USER_SESSION_ID == '':
+            p1_footer_end = bottom_end
+        else:
+            p2_start = html.find('<article', p1_footer_start)
+            # str.find returns -1 if not found, so this arithmetic is sloppy
+            p2_end = html.find('</article>', p1_footer_start) + len('</article>')
+            if not p2_start:
+                p1_footer_end = bottom_end
+            else:
+                p1_footer_end = p2_start - 1
+                p2_footer_end = bottom_end
+
+                p2_footer_start = p2_end + 1
+                output['part2'] = html[p2_start:p2_end]
+                output['part2_footer'] = html[p2_footer_start:p2_footer_end]
+        output['part1_footer'] = html[p1_footer_start:p1_footer_end]
+
+    # Download the input data if we can
+    if USER_SESSION_ID != '':
+        input = download_helper(baseurl + str(year) + '/day/' + str(day), '/input',
+                                USER_SESSION_ID, USER_AGENT)    
+        if input:
+            output['input'] = input.rstrip('\n')
+
+    return output
